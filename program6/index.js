75158fe3057bd53e09524617aec44305290c21af
pranjuk03 <93126962+pranjuk03@users.noreply.github.com>
2022-05-06 09:34:55 +0530

    Update index.js

--- program6/index.js
+++ program6/index.js
@@ -1,4 +1,4 @@
-var str = '({"firstName":"Mohammed","lastName":"Shuaib"})';
+var str = '({"firstName":"Pranjali","lastName":"Karjawkar"})';
 var obj = eval(str);
 document.write("String Is Converted Into Object Using Eval() : ")
 document.write(obj.firstName + " "); 
